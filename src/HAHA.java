ba7b0c69486fcad46cfe053f5b51f6abdf4ea849
Lu <l13402969069@163.com>
2022-11-22 14:28:35 +0800

    Initial commit

---
+++ src/HAHA.java
@@ -0,0 +1,13 @@
+package src;
+
+/**
+ * @author Ting.Lu date 2022-11-22
+ */
+public class HAHA {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
+    }
+}
